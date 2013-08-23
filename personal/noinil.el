f6fcdc5fe5f2c29243a77c181cdee09dea5b502d
Cheng Tan <noinil@gmail.com>
2013-08-23 10:57:06 +0800

    personal/noinil.el first commit

---
+++ personal/noinil.el
@@ -0,0 +1,57 @@
+;;; package --- Summary
+;;; noinil@gmail.com my personalize.el for prelude
+
+;;; commentary:
+
+;;; code:
+
+;; set line width
+(setq default-fill-column 80)
+
+;; set line number mode
+(global-linum-mode 1)
+
+;; set font for all windows
+(setq face-font-rescale-alist '(("Microsoft Yahei" . 1.2) ("WenQuanYi Zen Hei" . 1.2)))
+(add-to-list 'default-frame-alist '(font . "DejaVu Sans Mono-10"))
+
+;; calendar start on Monday
+(setq calendar-week-start-day 1)
+
+
+;; eim settings =================================================
+(add-to-list 'load-path "~/.emacs.d/elpa/eim-2011")
+(autoload 'eim-use-package "eim" "Another Emacs input method")
+(setq eim-use-tooltip nil)
+
+(register-input-method
+ "eim-wb" "euc-cn" 'eim-use-package
+ "五笔" "汉字五笔输入法" "wb.txt")
+(register-input-method
+ "eim-py" "euc-cn" 'eim-use-package
+ "拼音" "汉字拼音输入法" "py.txt")
+
+;; 用 ; 暂时输入英文
+(require 'eim-extra)
+(global-set-key ";" 'eim-insert-ascii)
+;; eim settings =================================================
+
+
+;; erc settings =================================================
+(setq erc-email-userid "noinil@gmail.com")
+(setq erc-nick "oinil")
+(setq erc-nick-uniquifier "_nil")
+(setq erc-server "irc.freenode.net")
+;; erc settings =================================================
+
+
+;; org-mode settings =================================================
+(setq org-todo-keywords
+      '((sequence "TODO(t)" "FEEDBACK(e!)" "VERIFY(j)" "STARTED(g!)" "|" "DONE(d!)")
+        (sequence "NEXT(n)" "SPECIFIED(i!)")
+        (sequence "SUBMITTED(s!)" "REVISION(v)" "|" "ACCEPTED(a!)" "PUBLISHED(p!)")
+        (sequence "REPORT(r@)" "BUG(b@)" "KNOWN-CAUSE(k@)" "|" "FIXED(f!)")
+        (sequence "WAITING(w)" "SOMEDAY(m)" "|" "CANCELED(c@)")))
+;; org-mode settings =================================================
+
+;;; noinil.el ends here
