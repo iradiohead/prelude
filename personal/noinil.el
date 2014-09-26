a31939107f09d2dfe8695f6de6f28fba71d381bd
Cheng Tan <noinil@gmail.com>
2014-09-26 18:05:11 +0900

    eim input methods removed after moved to MacOS

--- personal/noinil.el
+++ personal/noinil.el
@@ -67,20 +67,20 @@ noinil@gmail.com :: Cheng Tan ::
 ;; |  __/ | | | | | |
 ;;  \___|_|_| |_| |_|
 ;;
-(add-to-list 'load-path "/home/noinil/.eplugins/eim")
-(autoload 'eim-use-package "eim" "Another Emacs input method")
-(setq eim-use-tooltip nil)
+;; (add-to-list 'load-path "/home/noinil/.eplugins/eim")
+;; (autoload 'eim-use-package "eim" "Another Emacs input method")
+;; (setq eim-use-tooltip nil)
 
-(register-input-method
- "eim-wb" "euc-cn" 'eim-use-package
- "五笔" "汉字五笔输入法" "wb.txt")
-(register-input-method
- "eim-py" "euc-cn" 'eim-use-package
- "拼音" "汉字拼音输入法" "py.txt")
+;; (register-input-method
+ ;; "eim-wb" "euc-cn" 'eim-use-package
+ ;; "五笔" "汉字五笔输入法" "wb.txt")
+;; (register-input-method
+ ;; "eim-py" "euc-cn" 'eim-use-package
+ ;; "拼音" "汉字拼音输入法" "py.txt")
 
 ;; 用 ; 暂时输入英文
-(require 'eim-extra)
-(global-set-key ";" 'eim-insert-ascii)
+;; (require 'eim-extra)
+;; (global-set-key ";" 'eim-insert-ascii)
 ;;  ============================================================================
 
 ;; org-mode settings ===========================================================
